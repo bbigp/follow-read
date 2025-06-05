3acade1831dc8a01186d97d5901af82f69f1b9f1
peipei <peipei@qq.com>
2025-06-05 22:47:43 +0800

    delete print

--- lib/features/presentation/widgets/home/folder_tile.dart
+++ lib/features/presentation/widgets/home/folder_tile.dart
@@ -16,7 +16,6 @@ class FolderTile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    print("重建folder ${folder.id}");
     return Column(children: [
       MainTile(provider: FolderProvider(folder, ref: ref)),
       Padding(
